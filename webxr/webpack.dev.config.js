a73060ae9372f6fa42e78240e7594b9ea2f7535c
Labura <ghassane20a@gmail.com>
2022-05-04 19:12:00 +0000

    first commit

--- webxr/webpack.dev.config.js
+++ webxr/webpack.dev.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 const merge = require("webpack-merge");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const common = require("./webpack.common.config");
 
 module.exports = merge(common, {
   node: {
-    fs: 'empty'
+    fs: "empty",
   },
   mode: "development",
   // no weird "eval" stuff, shows code relatively clear in dist/main.js
@@ -13,34 +13,38 @@ module.exports = merge(common, {
   // this is just the entry js that gets bundled
   entry: {
     vr: "./src/index.js",
-    ar: "./src/ar-script.js"
+    ar: "./src/ar-script.js",
   },
   output: {
     // the filename is the name of the bundled file
     filename: "[name].bundle.js",
     // dist is the folder name it gets exported to
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    https: true,
-    host: '0.0.0.0',
+    https: false,
+    host: "localhost",
     overlay: {
       warnings: false,
-      errors: true
-    }
+      errors: true,
+    },
   },
-  plugins: [new HtmlWebpackPlugin({
-    chunks: ['vr'],
-    template: "./src/index.html",
-    filename: "index.html",
-    // Injects file in the head of the html
-    inject: 'head'
-  }),
-  new HtmlWebpackPlugin({
-    chunks: ['ar'],
-    template: "./src/ar.html",
-    filename: "ar.html",
-    // Injects file in the head of the html
-    inject: 'head'
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: "./src/images/favicon.png",
+      chunks: ["vr"],
+      template: "./src/index.html",
+      filename: "index.html",
+      // Injects file in the head of the html
+      inject: "head",
+    }),
+    new HtmlWebpackPlugin({
+      favicon: "./src/images/favicon.png",
+      chunks: ["ar"],
+      template: "./src/ar.html",
+      filename: "ar.html",
+      // Injects file in the head of the html
+      inject: "head",
+    }),
+  ],
 });
